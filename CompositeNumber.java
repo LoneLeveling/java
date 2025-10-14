3b2c3d47b36c1cd36fd5cddbcf4e1a2e2c2e0981
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-10-14 14:38:36 +0530

    Added new code file

---
+++ CompositeNumber.java
@@ -0,0 +1,13 @@
+//Number with more than 2 factors = composite
+public class CompositeNumber {
+    public static void main(String[] args)
+    {
+    int n =10;
+    for (int i=2;i<n;i++)
+        {
+            if (n%i==0)
+                System.out.println(n+" : is composite number");
+            break;
+        }
+    }
+}

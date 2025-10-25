db9ae1150dc3acac96570f79bfa8edef7ec69e8d
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-10-25 15:49:14 +0530

    added lc1716

---
+++ src/LC1716.java
@@ -0,0 +1,16 @@
+public class LC1716 {
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(totalMoney(n));
+    }
+
+    public static int totalMoney(int n) {
+        int fullWeek = n / 7;
+        int sumOfFullWeek = (7 * (fullWeek) * (fullWeek + 7)) / 2;
+
+        int remDays = n % 7;
+        int sumOfPartialWeek = (int)((remDays /2.0) * (2 * (fullWeek + 1) + (remDays - 1)));
+        int fullDaysSsum = sumOfFullWeek + sumOfPartialWeek;
+        return fullDaysSsum;
+    }
+}

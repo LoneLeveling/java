049ea7b29e6449cf98c4edf00ec46d5383d7dbd3
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-10-27 23:13:28 +0530

    added notes to leetocde 202 problm

--- src/LC202.java
+++ src/LC202.java
@@ -7,10 +7,10 @@ public class LC202
     int n=123;
         HashSet<Integer> seenNumber = new HashSet<>();//used hashset since it stores uniq values
         while (n!=1&&!seenNumber.contains(n))//checking if n!=1 and number is not present in the hashset "seenNumber"
-            //is number is present already then we quit the while loop and return false, cuz only the numbers whose sum of digits end as being 1 are happy number
+            //if number is present already then we quit the while loop and return false, cuz only the numbers whose sum of digits end as being 1 are happy number
         {
-            seenNumber.add(n);//calling helper function
-           n=getSumOfSquares(n);
+            seenNumber.add(n);//add new unique value to hashet
+           n=getSumOfSquares(n);//calling helper function
         }
 
         if (n==1) System.out.println(true);

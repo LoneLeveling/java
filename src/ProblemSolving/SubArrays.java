c6e03c4f2e9208ff69978645b59efa7859046d03
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-03-13 13:55:14 +0530

    subarray code

--- src/ProblemSolving/SubArrays.java
+++ src/ProblemSolving/SubArrays.java
@@ -2,7 +2,25 @@ package ProblemSolving;
 
 public class SubArrays {
     public static void main(String[] args) {
-
+        int[] arr ={2,3,-2,4};
+        int cnt=1;
+        int maxProduct=arr[0];
+//        int product=1;
+        for(int start=0;start<arr.length;start++)
+        {
+            for (int end=start;end<arr.length;end++)
+            {
+                int product = 1;
+                for (int j = start; j <= end; j++) {
+                    System.out.print(arr[j] + " ");
+                    product = product * arr[j];
+                }
+                maxProduct = Math.max(product, maxProduct);
+                System.out.print("------>" + product);
+                System.out.println();
+            }
+        }
+        System.out.println(maxProduct);
     }
 }
 

a9e736545259d6f14650391af73210e205d5a293
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-10 05:35:49 +0530

    code for hashmap

---
+++ src/ProblemSolving/HashMapFre.java
@@ -0,0 +1,15 @@
+package ProblemSolving;
+
+import java.util.HashMap;
+
+public class HashMapFre {
+    public static void main(String[] args)
+    {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Apple".toLowerCase(),10);
+        System.out.println(map.getOrDefault("apple",0));
+        System.out.println(map.getOrDefault("Banana",0));
+
+
+    }
+}
